e24f999a9e59d41de271f1c592ee7f63c44f7944
MicheleCavallaro97 <73616521+MicheleCavallaro97@users.noreply.github.com>
2021-06-28 12:25:45 +0200

    add album page in library

--- src/components/Home/Library/Album/Album.jsx
+++ src/components/Home/Library/Album/Album.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { getUserPlaylist,getPlaylistById } from '../../../../api/spotifyApi'
+import Lista from '../../Home_page/Show/Lista_brani/Lista_brani'
 const Album = ()=>{
-
-    return <div>Album</div>
+        const [albums,setAlbums] = useState([])
+    useEffect(()=>{
+        let array = []
+     getUserPlaylist()
+     .then(res=>getPlaylistById(res[0].id)
+                            .then(res=>res.map(elem=>{
+                                array.push(elem.track.album)
+                            }))
+                            .then(()=>{setAlbums(array)}))
+                            
+    },[])
+    return <div>
+        {albums.length>0?<Lista categoria={albums}></Lista>:<div>Error</div>}
+    </div>
 }
 export default Album
